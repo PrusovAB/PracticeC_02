079ee36bba2a5e4033b9f5715bfa87a9bcdad0c1
PrusovAB <djromeo93@mail.ru>
2022-07-13 15:23:33 +0300

    end

--- Exercise_10/Program.cs
+++ Exercise_10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+double SecondNumber(int number)
+{
+	double resultSecond = -1;
+	if (number < 100 || number > 999)
+	{
+		Console.WriteLine("Error: Введите трехзначное число");
+	}
+	else{
+		double result = number / 10;
+		resultSecond = Math.Round(result,1)%10;
+	}
+	return resultSecond;
+}
+
+Console.WriteLine(SecondNumber(numberOne));
