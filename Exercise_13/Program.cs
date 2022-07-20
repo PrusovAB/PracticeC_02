9be407317eabe909411768bd2c1376a3be16c2b6
Prusov A V <105492820+PrusovAB@users.noreply.github.com>
2022-07-20 23:36:24 +0300

    Update Program.cs
    Изменил добавил возможность проверять отрицательные числа

--- Exercise_13/Program.cs
+++ Exercise_13/Program.cs
@@ -1,9 +1,16 @@
-﻿Console.Clear();
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.Clear();
 Console.WriteLine("Введите число: ");
 int numberThree = Convert.ToInt32(Console.ReadLine());
+if (numberThree < 0){
+    numberThree = numberThree * (-1);
+}
 int number = numberThree;
 
 double rez = -1;
+
 
 if (numberThree < 100){
 	Console.WriteLine($"В числе {numberThree} не возможности показать третье число");
@@ -12,6 +19,10 @@ if (numberThree < 100){
 		if(numberThree < 10000){
 			double num = ((numberThree % 100) / 10);
 			rez = Math.Round(num,1);
+			if(numberThree<1000){
+			    num = numberThree % 10;
+			    rez = Math.Round(num,1);
+			}
 		}
 		if(numberThree >= 10000){
 			numberThree = numberThree / 10;
@@ -23,8 +34,7 @@ if(rez >= 0){
 	Console.WriteLine($"В числе {number} третье число по счету {rez}");
 }
 
-
-
-
+  }
+}
 
 
